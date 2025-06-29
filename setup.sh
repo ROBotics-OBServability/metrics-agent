b311af468372577bac4f0084e8988bb09e49e683
Lorenzo Fanton <lorenzo.fanton@outlook.it>
2025-06-29 16:25:59 +0200

    [fix]  -y

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ echo "✅ Done"
 echo "📦 Installing Docker Engine...: https://docs.docker.com/engine/install/ubuntu/"
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get -y install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -31,7 +31,7 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo "✅ Done"
 
 ################################################################
